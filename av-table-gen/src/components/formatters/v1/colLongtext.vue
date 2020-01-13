4ab2cfa4fc2b60a6e0533077541cb76baa6b13d2
possibly1 <brad.clonan@hotmail.com>
2020-01-13 01:00:01 -0500

    Update colLongtext.vue

--- av-table-gen/src/components/formatters/v1/colLongtext.vue
+++ av-table-gen/src/components/formatters/v1/colLongtext.vue
@@ -1,16 +1,15 @@
 <template>
-   <div style="color:red;"> 
-    
-     <p v-if="!isEditing" @click="editThis"> {{truncatedText ? truncatedText : cellValue}}</p>
-      <input v-else type="text" :value="cellValue" @input="updateValue($event)">
-      <button v-show="isEditing" @click.prevent="editValue">Save</button>
-   </div>
+<div >
+   <p v-if="!isEditing" @click="editThis"> {{truncatedText ? truncatedText : cellValue}}</p>
+   <input v-else type="text" :value="cellValue" @input="updateValue($event)">
+   <button v-show="isEditing" @click.prevent="editValue">Save</button>
+</div>
 </template>
 
 <script>
-   export default {
-      name: "colLongtext",
-         props: {
+export default {
+   name: "colLongtext",
+   props: {
       formatterParams: {
          type: Object,
          default: function () {
@@ -20,60 +19,62 @@
       cellValue: {
          type: [String, Object, Array]
       },
-      columnObj : {
-         type : Object,
+      columnObj: {
+         type: Object,
          default: function () {
             return {}
          }
       },
       title: {
          type: String,
-         default : null,
+         default: null,
       },
-      field : {
-         type : String,
-         default : null
+      field: {
+         type: String,
+         default: null
       },
-      scope : {
-         type : String,
-         default : null
+      scope: {
+         type: String,
+         default: null
       }
    },
    data() {
       return {
          isEditing: false,
-         newValue : null
+         newValue: null
       }
    },
    methods: {
       editThis() {
          console.log("this")
-        this.isEditing = !this.isEditing
+         this.isEditing = !this.isEditing
       },
-      updateValue(e){
+      updateValue(e) {
          this.newValue = e.target.value;
       },
       editValue() {
          console.log("this")
-         
+
          var r = confirm("Press a button!");
-if (r == true) {
-   this.isEditing = !this.isEditing
-   this.$emit('saveDataChange', this.newValue)
-} else {
-  this.isEditing = !this.isEditing
-}
+         if (r == true) {
+            this.isEditing = !this.isEditing
+            this.$emit('saveDataChange', this.newValue)
+         } else {
+            this.isEditing = !this.isEditing
+         }
       }
    },
-   computed : {
-   truncatedText () {
-            const {formatterParams, cellValue} = this;
-            return !formatterParams.maxLength ?
-            false
-            : cellValue.substring(0, formatterParams.maxLength);
+   computed: {
+      truncatedText() {
+         const {
+            formatterParams,
+            cellValue
+         } = this;
+         return !formatterParams.maxLength ?
+            false :
+            cellValue.substring(0, formatterParams.maxLength);
 
-
-         },
+      },
    }
-   }
+}
 </script>
